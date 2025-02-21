0596f044bd76ab2320a1830c9971880fc3330e43
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-02-21 12:19:21 -0800

    add closing in file upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const messageInput = document.querySelector(".message-input")
 const sendMessageButton = document.querySelector("#send-message")
 const fileInput = document.querySelector("#file-input")
 const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
-
+const fileCancelButton = document.querySelector("#file-cancel");
 
 const API_KEY = "AIzaSyBYmGhZhJDq3yejaXoEjrFYSOpa0aAIoBc";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
@@ -63,6 +63,7 @@ const handleOutgoingMessage = (e) => {
     e.preventDefault()
     userData.message = messageInput.value.trim();
     messageInput.value = "";
+    fileUploadWrapper.classList.remove("file-uploaded")
 
     const messageContent = `<div class="message-text"></div>${userData.file.data ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="attachment" />` : ""}`;
 
@@ -117,6 +118,12 @@ fileInput.addEventListener("change", () => {
     }
     reader.readAsDataURL(file)
 })
+
+fileCancelButton.addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded")
+});
+
 
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
 document.querySelector('#file-upload').addEventListener("click", () => fileInput.click());
